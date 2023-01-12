c479516ceccb144f69778ffc6afe7306c400e685
sonsondei <122409055+sonsondei@users.noreply.github.com>
2023-01-11 22:40:19 -0500

    test

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ module "blog_sg" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id = module.vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
