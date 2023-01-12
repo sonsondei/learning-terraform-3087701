1aa87471c144489d18224608e8f8d34ac9abbb94
sonsondei <122409055+sonsondei@users.noreply.github.com>
2023-01-11 23:38:30 -0500

    array fix

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "alb" {
 
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
