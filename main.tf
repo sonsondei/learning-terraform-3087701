5813e05aba3e7888a4545ef971cdda6811164489
sonsondei <122409055+sonsondei@users.noreply.github.com>
2023-01-11 21:51:17 -0500

    test3

--- main.tf
+++ main.tf
@@ -36,8 +36,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
   name = "blog"
-  
-  vpc_id = module.vpc.public_subnets[0]
+
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
