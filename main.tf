162ac95843eeb7e441f28b77d6301ef62e0eb784
sonsondei <122409055+sonsondei@users.noreply.github.com>
2023-01-11 23:53:24 -0500

    remove alb target

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.vpc.public_subnets
   target_group_arns = module.alg.target_group_arns
-  security_group_ids = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -80,12 +80,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
